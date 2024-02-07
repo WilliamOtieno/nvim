0ccbb4d57deedb51ddd3d374fca5ad0fcb8b3499
williamotieno <jimmywilliamotieno@gmail.com>
2024-02-08 01:15:04 +0300

    fix conflict btn copilot and cmp

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.copilot_assume_mapped = true
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
