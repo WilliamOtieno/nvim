633a68bccd7bbc4ab780f1ef849cde0500ee4a2d
williamotieno <jimmywilliamotieno@gmail.com>
2024-03-15 19:05:54 +0300

    minor

--- init.lua
+++ init.lua
@@ -95,17 +95,11 @@ require('lazy').setup({
     },
   },
   {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-        theme = "hyper"
-      }
-    end,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons'
-    }
+      'goolord/alpha-nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function ()
+          require'alpha'.setup(require'alpha.themes.startify'.config)
+      end
   },
   {
     -- Autocompletion
