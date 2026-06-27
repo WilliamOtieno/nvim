485f80fbf204072497d14e12199014a92bb64118
williamotieno <jimmywilliamotieno@gmail.com>
2026-06-27 15:42:45 +0300

    pre-commit + folding

--- init.lua
+++ init.lua
@@ -169,6 +169,10 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Enable folding
+vim.o.foldmethod = "indent"
+vim.o.foldlevel = 99
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -787,7 +791,7 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "black" },
+				python = { "pre-commit run" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
